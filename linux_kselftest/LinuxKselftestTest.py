22593aa3ebad9e7bf1974cb65ccf7f1001c3c858
Ryan Campbell <ryanjcampbell@google.com>
2017-03-01 15:57:32 -0800

    Update base test class.
    Update base test class to BaseTestClass.
    
    Test: pass
    Bug: 30895907
    Change-Id: Ib1df32188d9a967b99d115972f2c84c94f631425

--- linux_kselftest/LinuxKselftestTest.py
+++ linux_kselftest/LinuxKselftestTest.py
@@ -19,7 +19,7 @@ import logging
 import os
 
 from vts.runners.host import asserts
-from vts.runners.host import base_test_with_webdb
+from vts.runners.host import base_test
 from vts.runners.host import const
 from vts.runners.host import keys
 from vts.runners.host import test_runner
@@ -27,7 +27,7 @@ from vts.utils.python.controllers import android_device
 
 from vts.testcases.kernel.linux_kselftest import kselftest_config as config
 
-class LinuxKselftestTest(base_test_with_webdb.BaseTestWithWebDbClass):
+class LinuxKselftestTest(base_test.BaseTestClass):
     """Runs Linux Kselftest test cases against Android OS kernel.
 
     Attributes:

7154fb51c7d5cdb6fb40746bba3e19cb646d97f7
Steve Muckle <smuckle@google.com>
2017-10-24 17:54:41 -0700

    kselftest: add get_size test to staging
    Bug: 67016896
    Test: run vts-kernel -m VtsKernelLinuxKselftestStaging
    Change-Id: I2b861a28df666b77aaf9d3ec345cf874655e16da

--- linux_kselftest/kselftest_config.py
+++ linux_kselftest/kselftest_config.py
@@ -63,6 +63,7 @@ KSFT_CASES_STAGING = map(lambda x: test_case.LinuxKselftestTestcase(*(x)), [
     ("exec/execveat.sh", ["arm", "x86"], [32, 64]),
     ("kcmp/kcmp_test", ["arm", "x86"], [32, 64]),
     ("seccomp/seccomp_bpf", ["arm", "x86"], [32, 64]),
+    ("size/get_size", ["arm", "x86"], [32, 64]),
     ("timers/alarmtimer-suspend", ["arm", "x86"], [32, 64]),
     ("x86/single_step_syscall", ["x86"], [32, 64]),
     ("x86/sysret_ss_attrs", ["x86"], [32]),

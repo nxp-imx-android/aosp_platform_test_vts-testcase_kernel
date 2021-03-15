c7d069997e6252c6d0354aa5203cada49c129c67
Gregory Montoir <gregory.montoir@gmail.com>
2021-03-15 11:14:15 +0800

    KernelApiSysfsTest: only enforce testRtcHctosys if RTC is present
    Bug: 182426237
    Test: vts-tradefed run vts-kernel -m KernelApiSysfsTest
    Change-Id: I597f9af53f09b1bf25d7620ce63b7ed55c5a32ad

--- api/sysfs/src/com/android/tests/sysfs/KernelApiSysfsTest.java
+++ api/sysfs/src/com/android/tests/sysfs/KernelApiSysfsTest.java
@@ -18,6 +18,7 @@ package com.android.tests.sysfs;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeTrue;
 
 import com.android.tradefed.device.ITestDevice;
 import com.android.tradefed.testtype.DeviceJUnit4ClassRunner;
@@ -184,10 +185,11 @@ public class KernelApiSysfsTest extends BaseHostJUnit4Test {
         }
     }
 
-    /* Check that at least one rtc exists with hctosys = 1. */
+    /* If RTC is present, check that at least one rtc exists with hctosys = 1. */
     @Test
     public void testRtcHctosys() throws Exception {
         String[] rtcList = findFiles("/sys/class/rtc", "rtc*");
+        assumeTrue("Device has RTC", rtcList.length != 0);
         for (String entry : rtcList) {
             String content = getDevice().pullFileContents(entry + "/hctosys");
             if (Strings.isNullOrEmpty(content)) {
